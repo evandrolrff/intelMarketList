30f19ec606f9dd487ad1f9b2937a6d57b6d55555
Evandro <evandrolrff@gmail.com>
2021-05-17 20:24:20 -0300

    Modificações recentes

--- app/src/main/java/com/example/intelmarketlist/MainActivity.java
+++ app/src/main/java/com/example/intelmarketlist/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.intelmarketlist;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.actionbar_title);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
     }
 }
