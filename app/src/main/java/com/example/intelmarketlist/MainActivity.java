ba99a9d94ff614487159f350332137ab13915bd0
Evandro <evandrolrff@gmail.com>
2021-05-19 17:18:54 -0300

    Adiciona o botão fab e um toast para quando pressionado

--- app/src/main/java/com/example/intelmarketlist/MainActivity.java
+++ app/src/main/java/com/example/intelmarketlist/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(R.string.actionbar_title);
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#7729E6")));
+
+
     }
 
     @Override
