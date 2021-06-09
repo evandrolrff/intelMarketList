e9acf7a420c73cd1a5eaac3bad9aa2131c76d724
Evandro <evandrolrff@gmail.com>
2021-06-09 08:55:43 -0300

    remove floating button e adiciona nova opção no menu

--- app/src/main/java/com/example/intelmarketlist/MainActivity.java
+++ app/src/main/java/com/example/intelmarketlist/MainActivity.java
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(R.string.actionbar_title);
