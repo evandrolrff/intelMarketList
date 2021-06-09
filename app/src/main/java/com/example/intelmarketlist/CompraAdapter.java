593a6a40e1a2c374ce3b429fb6be32ebd2d13e9d
Evandro <evandrolrff@gmail.com>
2021-06-09 11:58:30 -0300

    Corrigido alguns warnings

--- app/src/main/java/com/example/intelmarketlist/CompraAdapter.java
+++ app/src/main/java/com/example/intelmarketlist/CompraAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class CompraAdapter extends RecyclerView.Adapter<CompraAdapter.ComprasViewHolder> {
-    protected static final String TAG = "teste";
     private final List<Compras> compras;
     private final Context context;
     private final CompraOnClickListener onclickListener;
@@ -31,8 +29,7 @@ public class CompraAdapter extends RecyclerView.Adapter<CompraAdapter.ComprasVie
     public ComprasViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType){
         View view = LayoutInflater.from(context).inflate(R.layout.adapter_compra, viewGroup, false);
 
-        ComprasViewHolder holder = new ComprasViewHolder(view);
-        return holder;
+        return new ComprasViewHolder(view);
     }
 
     @Override
@@ -65,8 +62,8 @@ public class CompraAdapter extends RecyclerView.Adapter<CompraAdapter.ComprasVie
         public ComprasViewHolder(View view){
             super(view);
             this.view = view;
-            tNome = (TextView) view.findViewById(R.id.tNome);
-            tValor = (TextView) view.findViewById(R.id.tValor);
+            tNome = view.findViewById(R.id.tNome);
+            tValor = view.findViewById(R.id.tValor);
         }
     }
 }
